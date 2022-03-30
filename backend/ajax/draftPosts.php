8ac52fcb468029aae605d75fa4dcdf0d8d1e8f3a
Joseph Clark <weirdlookingjay@live.com>
2022-03-30 14:51:55 -0400

    Draft Posts

---
+++ backend/ajax/draftPosts.php
@@ -0,0 +1,28 @@
+<?php
+include '../init.php';
+
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    if (isset($_POST['postIDs'])) {
+        $postIDs = json_decode($_POST['postIDs']);
+        $blogIDs = json_decode($_POST['blogIDs']);
+        if (!empty($postIDs) && !empty($blogIDs)) {
+            foreach ($postIDs as $postID) {
+                foreach ($blogIDs as $blogID) {
+                    $post = $userObj->get('posts', ['postID' => $postID, 'blogID' => $blogID]);
+                    $blog = $dashObj->blogAuth($post->blogID);
+                    if ($blog) {
+                        if ($blog->role === "Admin" OR $blog->userID === $post->authorID) {
+                            $userObj->update('posts', ['postStatus' => 'draft'], ['postID' => $post->postID, 'blogID' => $blog->blogID]);
+                        } else {
+                            echo "You don't have rights to perform this action!";
+                            break;
+                        }
+                    } else {
+                        echo "Something went wrong!";
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
--- frontend/assets/js/postStatus.js
+++ frontend/assets/js/postStatus.js
@@ -1,4 +1,47 @@
 var publishBtn = document.querySelector("#publishBtn");
+var draftBtn = document.querySelector("#draftBtn");
+
+draftBtn.addEventListener("click", function(e) {
+    var checkBox = document.querySelectorAll(".postCheckBox");
+    var postIDs = new Array();
+    var blogIDs = new Array();
+
+    checkBox.forEach(function(el) {
+        if(el.checked) {
+            postIDs.push(el.value);
+            blogIDs.push(el.dataset.blog);
+        }
+    });
+
+    if(postIDs.length > 0) {
+        if(confirm("Are you sure you want to draft this?")) {
+            var formData = new FormData();
+
+            formData.append("postIDs", JSON.stringify(postIDs));
+            formData.append("blogIDs", JSON.stringify(blogIDs));
+
+            var httpRequest = new XMLHttpRequest();
+
+            if (httpRequest) {
+                httpRequest.open('POST', 'https://blog-coder.herokuapp.com/backend/ajax/draftPosts.php', true);
+                //httpRequest.open('POST', 'http://herokublog.local/backend/ajax/draftPosts.php', true);
+                httpRequest.onreadystatechange = function () {
+                    if (this.readyState === 4 && this.status === 200) {
+                        if(this.responseText.length != 0) {
+                            alert(this.responseText);
+                        }
+                        location.reload(true);
+                    }
+                }
+                httpRequest.send(formData);
+            }
+        }
+    } else {
+        alert("No Posts are selected!");
+        location.reload(true);
+    }
+
+});
 
 publishBtn.addEventListener("click", function(event) {
     var checkBox = document.querySelectorAll(".postCheckBox");
