9fa70fe4877d4aa26677d17aca60b767fd3d7ac7
Joseph Clark <weirdlookingjay@live.com>
2022-04-05 10:03:35 -0400

    Update Dashboard.php

--- backend/classes/Dashboard.php
+++ backend/classes/Dashboard.php
@@ -200,7 +200,7 @@ class Dashboard
 
 	public function getAllComments($offset, $limit, $type, $blogID)
 	{
-		$sql = "SELECT * FROM users, herokublogger.comments 
+		$sql = "SELECT * FROM users, comments 
 						 LEFT JOIN posts ON posts.postID=comments.postID
 						 WHERE users.userID = posts.authorID 
 						 AND comments.blogID =:blogID AND comments.status =:type
