4943698c6a7c80306cb2070982b78d919c99fc8c
Joseph Clark <weirdlookingjay@live.com>
2022-04-07 15:43:22 -0400

    Update updateDescription.php

--- backend/ajax/updateDescription.php
+++ backend/ajax/updateDescription.php
@@ -1,19 +1,19 @@
 <?php
-include "../init.php";
-
-if($_SERVER['REQUEST_METHOD'] === "POST") {
-	if(isset($_POST['description'])) {
-		$description = Validate::escape($_POST['description']);
-		$blogID = (int) $_POST['blogID'];
+include '../init.php';
+if($_SERVER['REQUEST_METHOD'] === "POST"){
+	if(isset($_POST['description'])){
+		$title =Validate::escape($_POST['description']);
+		$blogID =(int)$_POST['blogID'];
 		$blog = $dashObj->blogAuth($blogID);
 
-		if($blog) {
-			if($blog->role === "Admin") {
-				if(!empty($title)) {
-					$userObj->update('blogs', ['Description' => $description], ['blogID' => $blog->blogID]);
+		if($blog){
+			if($blog->role === "Admin"){
+				if(!empty($title)){
+					$userObj->update('blogs',['Description'=>$title],['blogID'=>$blog->blogID]);
 				}
-			} else {
-				echo "Owner Error";
+
+			}else{
+				echo "OwnnerError";
 			}
 		}
 	}
