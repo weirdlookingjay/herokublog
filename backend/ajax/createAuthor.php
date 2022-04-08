e13540dc4a220d1846557b460828b0c6486cf5e9
Joseph Clark <weirdlookingjay@live.com>
2022-04-08 13:55:43 -0400

    Updated files

--- backend/ajax/createAuthor.php
+++ backend/ajax/createAuthor.php
@@ -1,46 +1,89 @@
 <?php
-include "../init.php";
-
-if($_SERVER['REQUEST_METHOD'] === "POST") {
-	if(isset($_POST['email'])) {
-		$blogID = (int) $_POST['blogID'];
-		$email = Validate::escape($_POST['email']);
-		$name = Validate::escape($_POST['name']);
-		$pass = $_POST['pass'];
+include '../init.php';
+if($_SERVER['REQUEST_METHOD']==="POST"){
+	if(isset($_POST['email'])){
+		$blogID =(int) $_POST['blogID'];
+		$email  =Validate::escape($_POST['email']);
+		$name   =Validate::escape($_POST['name']);
+		$pass   = $_POST['pass'];
 		$passRe = $_POST['passRe'];
-		$blog = $dashObj->blogAuth($blogID);
-
-		if(!empty($email) && !empty($name) && !empty($pass) && !empty($passRe)) {
-			if(Validate::filterEmail($email)) {
-				if(!$userObj->emailExist($email)) {
-					if($pass === $passRe) {
-						$password = $userObj->hash($pass);
-						if(!empty($_FILES['file']['name'][0])) {
-							// Upload Image
+		$blog   = $dashObj->blogAuth($blogID);
+		if(!empty($email) && !empty($name) && !empty($pass) && !empty($passRe)){
+			if(Validate::filterEmail($email)){
+				if(!$userObj->emailExist($email)){
+					if($pass ===$passRe){
+						$password =$userObj->hash($pass);
+						if(!empty($_FILES['file']['name'][0])){
 							$image = $userObj->uploadImage($_FILES['file']);
-							if(!$image) {
+							if(!$image){
 								echo $userObj->imageError();
 								exit;
 							}
-						} else {
-							$image = "frontend/assets/images/avatar.png";
+						}else{
+							$image ="frontend/assets/images/avatar.png";
 						}
-
-						if($blog->role === "Admin") {
-							$userID = $userObj->create('users', ['email' => $email, 'fullName' => $name, 'password' => $password, 'profileImage' => $image]);
-							$userObj->create('blogsAuth', ['blogID' => $blog->blogID, 'userID' => $userID, 'role' => 'Author']);
-						} else {
-							echo "You dont have rights to perform this action!";
+						if($blog->role ==="Admin"){
+							$userID=$userObj->create('users',['email'=>$email,'fullName'=>$name,'password'=>$password,'profileImage'=>$image]);
+							$userObj->create('blogsAuth',['blogID'=>$blog->blogID,'userID'=>$userID,'role'=>'Author']);
+						}else{
+							echo"Errore, non sei un Amministratore";
 						}
-					} else {
-						echo "password does not match!";
+					}else{
+						Echo "Le password non corrispondono";
 					}
-				} else {
-					echo "Email Address is already in use!";
+				}else{
+					echo "Email già in uso";
 				}
-			} else {
-				echo "Invalid Email Format!";
+
+			}else{
+				echo "Errore formato email";
 			}
 		}
 	}
 }
+
+//include "../init.php";
+//
+//if($_SERVER['REQUEST_METHOD'] === "POST") {
+//	if(isset($_POST['email'])) {
+//		$blogID = (int) $_POST['blogID'];
+//		$email = Validate::escape($_POST['email']);
+//		$name = Validate::escape($_POST['name']);
+//		$pass = $_POST['pass'];
+//		$passRe = $_POST['passRe'];
+//		$blog = $dashObj->blogAuth($blogID);
+//
+//		if(!empty($email) && !empty($name) && !empty($pass) && !empty($passRe)) {
+//			if(Validate::filterEmail($email)) {
+//				if(!$userObj->emailExist($email)) {
+//					if($pass === $passRe) {
+//						$password = $userObj->hash($pass);
+//						if(!empty($_FILES['file']['name'][0])) {
+//							// Upload Image
+//							$image = $userObj->uploadImage($_FILES['file']);
+//							if(!$image) {
+//								echo $userObj->imageError();
+//								exit;
+//							}
+//						} else {
+//							$image = "frontend/assets/images/avatar.png";
+//						}
+//
+//						if($blog->role === "Admin") {
+//							$userID = $userObj->create('users', ['email' => $email, 'fullName' => $name, 'password' => $password, 'profileImage' => $image]);
+//							$userObj->create('blogsAuth', ['blogID' => $blog->blogID, 'userID' => $userID, 'role' => 'Author']);
+//						} else {
+//							echo "You dont have rights to perform this action!";
+//						}
+//					} else {
+//						echo "password does not match!";
+//					}
+//				} else {
+//					echo "Email Address is already in use!";
+//				}
+//			} else {
+//				echo "Invalid Email Format!";
+//			}
+//		}
+//	}
+//}
--- backend/classes/Users.php
+++ backend/classes/Users.php
@@ -124,34 +124,63 @@ class Users
         }
     }
 
-    public function uploadImage($file) {
-    	$fileInfo = getimagesize($file['tmp_name']);
-    	$fileTmp = $file['tmp_name'];
-    	$fileName = $file['name'];
-    	$fileSize = $file['size'];
-    	$errors = $file['error'];
+//    public function uploadImage($file) {
+//    	$fileInfo = getimagesize($file['tmp_name']);
+//    	$fileTmp = $file['tmp_name'];
+//    	$fileName = $file['name'];
+//    	$fileSize = $file['size'];
+//    	$errors = $file['error'];
+//
+//    	//get extension
+//	    $ext = explode('.', $fileName);
+//	    $ext = strtolower(end($ext));
+//
+//	    //extension types
+//	    $allowed = array('image/png', 'image/jpeg', 'image/jpg');
+//
+//	    if(in_array($fileInfo['mime'], $allowed)) {
+//	    	$folder = 'frontend/content/';
+//	    	$file = $folder.substr(md5(time().mt_rand()), 2,25).'.'.$ext;
+//
+//	    	if($errors === 0) {
+//	    		if($fileSize <= 10485760) {
+//	    			move_uploaded_file($fileTmp, $_SERVER['DOCUMENT_ROOT'].$file);
+//	    			return $file;
+//			    }
+//		    } else {
+//			    $this->imageError = "File is too large!";
+//		    }
+//	    } else {
+//	    	$this->imageError = "File extension is not allowed!";
+//	    }
+//    }
 
-    	//get extension
-	    $ext = explode('.', $fileName);
-	    $ext = strtolower(end($ext));
+	public function uploadImage($file){
+		$fileInfo = getimagesize($file['tmp_name']);
+		$fileTmp  = $file['tmp_name'];
+		$fileName = $file['name'];
+		$fileSize = $file['size'];
+		$errors   = $file['error'];
 
-	    //extension types
-	    $allowed = array('image/png', 'image/jpeg', 'image/jpg');
+		$ext = explode('.',$fileName);
+		$ext = strtolower(end($ext));
 
-	    if(in_array($fileInfo['mime'], $allowed)) {
-	    	$folder = 'frontend/content/';
-	    	$file = $folder.substr(md5(time().mt_rand()), 2,25).'.'.$ext;
+		$allowed = array('image/png','image/jpg','image/jpeg');
 
-	    	if($errors === 0) {
-	    		if($fileSize <= 10485760) {
-	    			move_uploaded_file($fileTmp, $_SERVER['DOCUMENT_ROOT'].$file);
-	    			return $file;
-			    }
-		    } else {
-			    $this->imageError = "File is too large!";
-		    }
-	    } else {
-	    	$this->imageError = "File extension is not allowed!";
-	    }
-    }
+		if(in_array($fileInfo['mime'],$allowed)){
+			$folder ='frontend/content/';
+			$file = $folder.substr(md5(time().mt_rand()),2,25).'.'.$ext;
+
+			if($errors ===0){
+				if($fileSize <= 10485760 ){
+					move_uploaded_file($fileTmp,$_SERVER['DOCUMENT_ROOT'].'/corso/Blogger/'.$file);
+					return $file;
+				}else {
+					$this->imageError ="Dimensione immagine troppo grande";
+				}
+			}else{}
+		}else{
+			$this->imageError ="Estensione immagine non permessa";
+		}
+	}
 }
